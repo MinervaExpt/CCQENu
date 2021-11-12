169a1f397eeca33cee07069320b5c4bf9dfc26b1
Sean Gilligan <gilligan@minervagpvm01.fnal.gov>
2021-11-12 15:54:53 -0600

    first add of mateus neutrino json files

--- make_hists/smg/mnv_setup.sh
+++ make_hists/smg/mnv_setup.sh
@@ -1,27 +1,17 @@
-# unix version
-
-
-export CVSROOT=minervacvs@cdcvs.fnal.gov:/cvs/mnvsoft
-
-#source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
-#setup cmake v3_14_3 -z /cvmfs/larsoft.opensciencegrid.org/products
-#setup root v6_16_00 -q e17:prof -z /cvmfs/larsoft.opensciencegrid.org/products
-#setup python v3_7_2 -z /cvmfs/larsoft.opensciencegrid.org/products
-#setup gdb  v8_2_1 -z /cvmfs/larsoft.opensciencegrid.org/products
-
-source /cvmfs/minerva.opensciencegrid.org/minerva/hep_hpc_products/setups
-setup root v6_10_04d -q e14:prof
-setup cmake v3_7_1
-
-#source /home/physics/gilligas/conda.sh
-
 export WHEREIPUTMYCODE=/minerva/app/users/$USER/minerva
+export CCQEMAT=$WHEREIPUTMYCODE/CCQENu/make_hists
+export CVSROOT=minervacvs@cdcvs.fnal.gov:/cvs/mnvsoft
+export JSONCPP_DIR=/minerva/app/users/$USER/LocalApps/jsoncpp-build
+export LD_LIBRARY_PATH=${JSONCPP_DIR}/lib:${PLOTUTILSROOT}:${UNFOLDUTILSROOT}:${LD_LIBRARY_PATH}
+export THEDATA=/pnfs/minerva/persistent/users/drut1186/CCQENu_Anatuples/MuonKludge_ProtonLLR_UpdatedNeutron
+
+# Same cvmfs setup/cmake/root used for MAT-MINERvA
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
+setup cmake v3_14_3 -z /cvmfs/larsoft.opensciencegrid.org/products
+setup root v6_16_00 -q e17:prof -z /cvmfs/larsoft.opensciencegrid.org/products
+
+kx509
+voms-proxy-init -rfc --voms=fermilab:/fermilab/minerva/Role=Analysis --noregen -valid 24:00
+
 source $WHEREIPUTMYCODE/opt/build/setup.sh
 
-export JSONCPP_DIR=/minerva/app/users/$USER/LocalApps/jsoncpp-build
-
-export CCQEMAT=$WHEREIPUTMYCODE/CCQENu/make_hists
-
-#export DYLD_LIBRARY_PATH=${PLOTUTILSROOT}
-
-export THEDATA=/minerva/data/ME/MuonKludge_ProtonLLR_UpdatedNeutron
