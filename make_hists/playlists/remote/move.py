9534a8886434ed5d991ecb972384192f3c5b92f7
Heidi Schellman <Heidi.Schellman@oregonstate.edu>
2021-11-16 13:13:27 -0800

    script to move files

---
+++ make_hists/playlists/remote/move.py
@@ -0,0 +1,12 @@
+import os,sys,string
+oldpath = "root://fndca1.fnal.gov:1094/pnfs/fnal.gov/usr/minerva/persistent/users/drut1186/CCQENu_Anatuples/"
+newpath = "/minerva/data/ME/"
+f = open(sys.argv[1],'r')
+g = open(sys.argv[1].replace(".txt",".sh"),'w')
+files = f.readlines()
+for file in files:
+    newloc = file.replace(oldpath,newpath)
+    newline = file.replace("root:","xrdcopy root:")
+    newline = newline.replace(".root",".root "+newloc)
+    g.write(newline)
+    print (newline)
