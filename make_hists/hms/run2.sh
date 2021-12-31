d536f76a8b88e1b30657d3df56f5965a8fec8833
Heidi Schellman <heidi.schellman@oregonstate.edu>
2021-12-31 13:16:38 -0800

    new json to run background studies

---
+++ make_hists/hms/run2.sh
@@ -0,0 +1,4 @@
+./sidebands_v2 AntiNu_6D 1 >& 6Drun.log
+./sidebands_v2 AntiNu_6E 1 >& 6Erun.log
+./sidebands_v2 AntiNu_6F 1 >& 6Frun.log
+./sidebands_v2 AntiNu_6G 1 >& 6Grun.log
---
+++ make_hists/hms/run3.sh
@@ -0,0 +1,5 @@
+./sidebands_v2 AntiNu_5A 1 >& 5Arun.log
+./sidebands_v2 AntiNu_6H 1 >& 6Hrun.log
+./sidebands_v2 AntiNu_6I 1 >& 6Irun.log
+./sidebands_v2 AntiNu_6J 1 >& 6Jrun.log
+#./sidebands_v2 AntiNu_5A 1 >& 5Arun.log
