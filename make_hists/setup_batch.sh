9f5d94789f0770fa5db4c60b19b5efee9a7c2d12
Heidi Schellman <heidi.schellman@oregonstate.edu>
2022-05-28 16:41:14 -0700

    batch stuff

---
+++ make_hists/setup_batch.sh
@@ -0,0 +1,20 @@
+# batch version to run - assumes you are in the unfolded CCQEMAT directory
+source /cvmfs/fermilab.opensciencegrid.org/products/common/etc/setups
+
+export CVSROOT=minervacvs@cdcvs.fnal.gov:/cvs/mnvsoft
+
+# get the packages you need to run this
+setup cmake v3_14_3 -z /cvmfs/larsoft.opensciencegrid.org/products
+setup root v6_16_00 -q e17:prof -z /cvmfs/larsoft.opensciencegrid.org/products 
+setup boost v1_70_0 -q e19:prof -z /cvmfs/larsoft.opensciencegrid.org/products  # do we still need this? 
+setup python v3_7_2 -z /cvmfs/larsoft.opensciencegrid.org/products
+setup gdb  v8_2_1 -z /cvmfs/larsoft.opensciencegrid.org/products
+
+export WHEREIPUTMYCODE=$PWD    # change this here if you want a different location
+export MPARAMFILESROOT=/cvmfs/minerva.opensciencegrid.org/minerva/CentralizedFluxAndReweightFiles/MParamFiles
+export MPARAMFILES=$MPARAMFILESROOT/data
+#
+export CCQEMAT=$WHEREIPUTMYCODE/CCQENu/make_hists
+export JSONCPP_DIR=$WHEREIPUTMYCODE/jsoncpp-build # need to change this probably
+export LD_LIBRARY_PATH=$JSONCPP_DIR/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=$CCQEMAT/python:$WHEREIPUTMYCODE/MAT-MINERvA/python:$WHEREIPUTMYCODE/MAT-MINERvA/python/PlotUtils:$PYTHONPATH
--- utilities/SubmitJobsToGrid_MAT_CCQE.py
+++ utilities/SubmitJobsToGrid_MAT_CCQE.py
@@ -9,48 +9,55 @@ import datetime
 
 # HMS - modify to use dropbox and do cleaner tar
 
+tmpdir = "/minerva/app/users/$USER/"
+
 # Write the command you used to run your analysis
-def writeEventLoop(mywrapper,outdir):
-    mywrapper.write("./sidebands_v2 "+"config "\n")
+def writeEventLoop(mywrapper,config,outdir):
+    mywrapper.write("./sidebands_v2 "+config+ "\n")
     mywrapper.write("ifdh cp ./*.root "+outdir)
 def writeSetups(mywrapper,basedir):
     topdir = "$CONDOR_DIR_INPUT" + basedir
     mywrapper.write("cd "+topdir+"\n")
+    mywrapper.write("export WHEREIPUTMYCODE=%s"%topdir)
+    mywrapper.write("du")
     #mywrapper.write("cd Ana/PlotUtils/cmt\n")
     #mywrapper.write("cmt config\n")
-    mywrapper.write("source setup.sh\n")
+    mywrapper.write("source CCQENU/make_hists/setup_batch.sh\n")
     mywrapper.write("cd "+topdir+"\n")
     #mywrapper.write("cd Ana/Minerva101/MAT_Tutorial\n")
     #mywrapper.write("echo Rint.Logon: ./rootlogon_grid.C > ./.rootrc\n")
 
 # Define function to create tarball
 def createTarball(tardir,tag,basedir):
-    tmpdir = "/minerva/data/users/$USER/"
+    
     found = os.path.isfile("%s/myareatar_%s.tar.gz"%(tardir,tag))
     if(not found):
         #cmd = "tar -czf /minerva/app/users/$USER/myareatar_%s.tar.gz %s"%(tag,basedir)
-        cmd = "tar --exclude={*.git,*.root,*.png,*.pdf,*.gif} -zcf  %s/myareatar_%s.tar.gz %s"(tmpdir,tag,basedir)
-        print "Making tar",cmd
+        cmd = "tar --exclude={*.git,*.root,*.png,*.pdf,*.gif} -zcf  %s/myareatar_%s.tar.gz %s"%(tmpdir,tag,basedir)
+        print ("Making tar",cmd)
         os.system(cmd)
-        cmd2 = "cp /minerva/app/users/$USER/myareatar_%s.tar.gz %s/"%(tmpdir,tag,tardir)
-        print "Copying tar",cmd2
+        cmd2 = "cp %s/myareatar_%s.tar.gz %s/"%(tmpdir,tag,tardir)
+        print ("Copying tar",cmd2)
+        
         os.system(cmd2)
+        return "myareatar_%s.tar.gz"%(tag)
 # Define function to unpack tarball
 def writeTarballProceedure(mywrapper,tag,basedir):
-    print "I will be making the tarball upacking with this version" 
-    print "Path is",basedir
+    print ("I will be making the tarball upacking with this version")
+    print ("Path is",basedir)
     #mywrapper.write("source /cvmfs/minerva.opensciencegrid.org/minerva/software_releases/v22r1p1/setup.sh\n")
     mywrapper.write("cd $CONDOR_DIR_INPUT\n")
     mywrapper.write("env\n")
     mywrapper.write("tar -xvzf myareatar_%s.tar.gz\n"%tag)
-    mywrapper.write(
     mywrapper.write("cd $CONDOR_DIR_INPUT\n")
     writeSetups(mywrapper,basedir)
 
 def writeOptions(parser):
-    print "Now write options to the parser" 
+    print ("Now write options to the parser")
     # Directory to write output
     parser.add_option('--outdir', dest='outdir', help='Directory to write output to', default = "/pnfs/minerva/scratch/users/"+_user_+"/default_analysis_loc/")
+    parser.add_option('--config', dest='config', help='json config file name', default = "test_v9")
+    
     parser.add_option('--tardir', dest='tardir', help='Tarball location', default = "/pnfs/minerva/scratch/users/"+_user_+"/default_analysis_loc/")
     parser.add_option('--basedir', dest='basedir', help='Base directory for making tarball', default = "NONE")
     parser.add_option('--stage', dest='stage', help='Process type', default="NONE")
@@ -77,9 +84,9 @@ writeOptions(parser)
 
 # Now print some information for user's selected options
 if opts.mail:
-    print "############################################" 
-    print "You have opted to Get mails once the jobs are finished of failed." 
-    print "#############################################" 
+    print ("############################################")
+    print ("You have opted to Get mails once the jobs are finished of failed.")
+    print ("#############################################")
 
 ##############################################
 #  TODO: Here I want to put the tag scheme...
@@ -87,17 +94,17 @@ if opts.mail:
 tag_name = str(opts.tag)
 time_stamp = int(time.time())
 if tag_name=="tag_":
-    print "YOU DIDNT SPECIFY ANY ANY TAG...SO I WILL USE MY OWN TAGGING SCHEME" 
+    print ("YOU DIDNT SPECIFY ANY ANY TAG...SO I WILL USE MY OWN TAGGING SCHEME" )
     # You can add in your own tagging scheme
     tag_name += "default_" 
 
-print "Is everything fine till here*********" 
+print ("Is everything fine till here*********")
 # Add the time stamp to tag
 if not opts.notimestamp:
     tag_name += str(time_stamp)
 
-print "Tag for this version is ",tag_name
-print "******************************************************" 
+print ("Tag for this version is ",tag_name)
+print ("******************************************************")
 
 # This is the output directory after the job is finished
 output_dir = "$CONDOR_DIR_HISTS/" 
@@ -110,7 +117,7 @@ memory = 2000
 
 # Check the stage is valid or not
 if opts.stage not in valid_stages:
-    print opts.stage,"Selected stage is not valid. Here are the valid options",valid_stages
+    print (opts.stage,"Selected stage is not valid. Here are the valid options",valid_stages)
     sys.exit()
 
 ##############################################
@@ -123,14 +130,14 @@ mywrapper = open(wrapper_name,"w")
 mywrapper.write("#!/bin/sh\n")
 # Now create tarball
 if (opts.sametar==False):
-    createTarball(opts.tardir,tag_name,opts.basedir)
+    tarname = createTarball(opts.tardir,tag_name,opts.basedir)
 else:
     tarname = str(opts.tarfilename)
     if not os.path.exists(opts.tardir+opts.tarfilename):
-        print "Tar File "+opts.tardir+opts.tarfilename+" doesn't Exist!"
+        print ("Tar File "+opts.tardir+opts.tarfilename+" doesn't Exist!")
         sys.exit()
     #change the tag to the current one...
-    cmd="cp "+opts.tardir+opts.tarfilename+" "+opts.outdir+"myareatar_"+tag_name+".tar.gz"
+    cmd="cp "+opts.tardir+opts.tarfilename+" "+opts.tardir+"myareatar_"+tag_name+".tar.gz"
     os.system(cmd)
 
 # This will unpack the tarball we just made above
@@ -138,7 +145,7 @@ writeTarballProceedure(mywrapper,tag_name,opts.basedir)
 
 # Now the add the command to run event loop 
 if(opts.stage=="eventLoop"):
-    writeEventLoop(mywrapper,opts.outdir)
+    writeEventLoop(mywrapper,opts.config,opts.outdir)
 
 mywrapper.close()
 
@@ -152,7 +159,7 @@ gccstring = "x86_64-slc7-gcc49-opt"
 # Now add the execute command
 cmd = "" 
 cmd += "jobsub_submit --group minerva " #Group of experiment
-cmd += "--cmtconfig "+gccstring+" " #Setup minerva soft release built with minerva configuration
+#cmd += "--cmtconfig "+gccstring+" " #Setup minerva soft release built with minerva configuration
 cmd += "--OS sl7 " #Operating system #Not needed in SL7
 
 if opts.mail:
@@ -164,15 +171,15 @@ cmd += "--expected-lifetime 12h "
 cmd += "--memory "+str(memory)+"MB " 
 cmd += configstring+" " #the environments for the tunes to bee applied
 #cmd += "-f "+opts.outdir+"/myareatar_"+tag_name+".tar.gz "
-cmd += "--tar_file_name dropbox://"+opts.tardir+opts.tarfilename" --use-cvmfs-dropbox"
-cmd += "-i /cvmfs/minerva.opensciencegrid.org/minerva/software_releases/v22r1p1"+" " 
+cmd += "--tar_file_name dropbox://"+opts.tardir+"/myareatar_"+tag_name+".tar.gz  --use-cvmfs-dropbox "
+#cmd += "-i /cvmfs/minerva.opensciencegrid.org/minerva/software_releases/v22r1p1"+" "
 cmd += "file://"+os.environ["PWD"]+"/"+wrapper_name
-print cmd
+print (cmd)
 
 os.system(cmd)
 
-print "Deleting the app area tar..... " 
-os.system("rm -rf /minerva/app/users/$USER/myareatar_"+tag_name+".tar.gz")
-print "Sleeping" 
+print ("Deleting the app area tar..... ")
+os.system("rm  "+tmpdir+"myareatar_"+tag_name+".tar.gz")
+print ("Sleeping" )
 
 time.sleep(1)
