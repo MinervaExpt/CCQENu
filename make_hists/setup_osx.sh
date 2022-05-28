4f5c92893018b458049bc036130e139aa781269b
Heidi Schellman <heidi.schellman@oregonstate.edu>
2022-05-28 10:05:04 -0700

    add local python to path

--- make_hists/setup_osx.sh
+++ make_hists/setup_osx.sh
@@ -11,7 +11,7 @@ export JSONCPP_DIR=$HOME/LocalApps/jsoncpp-build # need to change this probably
 export CCQEMAT=$WHEREIPUTMYCODE/CCQENu/make_hists
 
 export DYLD_LIBRARY_PATH=${PLOTUTILSROOT}
-export PYTHONPATH=$WHEREIPUTMYCODE/MAT-MINERvA/python:$WHEREIPUTMYCODE/MAT-MINERvA/python/PlotUtils:$PYTHONPATH
+export PYTHONPATH=$CCQEMAT/python:$WHEREIPUTMYCODE/MAT-MINERvA/python:$WHEREIPUTMYCODE/MAT-MINERvA/python/PlotUtils:$PYTHONPATH
 export THEDATA=/pnfs/minerva/persistent/users/drut1186/CCQENu_Anatuples/MuonKludge_ProtonLLR_UpdatedNeutron
 
 sed s+GIT_COMMIT_HASH+`git rev-parse --verify HEAD`+ utils/gitVersion.h.in > utils/gitVersion.h
