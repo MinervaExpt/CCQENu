9dbf27dbdd14c102a8207872680256b104530f9e
Noah Harvey Vaughan <nhvaughan@users.noreply.github.com>
2022-06-02 11:49:19 -0700

    Noah's setup_osx

--- make_hists/setup_osx.sh
+++ make_hists/setup_osx.sh
@@ -3,13 +3,13 @@
 
 export CVSROOT=minervacvs@cdcvs.fnal.gov:/cvs/mnvsoft
 
-export WHEREIPUTMYCODE=$HOME/Dropbox/TOPMAT    # change this here if you want a different location
-source $WHEREIPUTMYCODE/opt/build/setup.sh     # you have to have this
+#export WHEREIPUTMYCODE=$HOME/Dropbox/TOPMAT    # change this here if you want a different location
+source /Users/vaughann/gitmat/opt/build/setup.sh     # you have to have this
 
-export JSONCPP_DIR=$HOME/LocalApps/jsoncpp-build # need to change this probably
+#export JSONCPP_DIR=$HOME/LocalApps/jsoncpp-build # need to change this probably
 
 export CCQEMAT=$WHEREIPUTMYCODE/CCQENu/make_hists
 
 export DYLD_LIBRARY_PATH=${PLOTUTILSROOT}
-export PYTHONPATH=$WHEREIPUTMYCODE/MAT-MINERvA/python:$WHEREIPUTMYCODE/MAT-MINERvA/python/PlotUtils:$PYTHONPATH
-export THEDATA=/pnfs/minerva/persistent/users/drut1186/CCQENu_Anatuples/MuonKludge_ProtonLLR_UpdatedNeutron
+export PYTHONPATH=${WHEREIPUTMYCODE}/MAT-MINERvA/python:${WHEREIPUTMYCODE}/MAT-MINERvA/python/PlotUtils:${CCQEMAT}/python:$PYTHONPATH
+#export THEDATA=/pnfs/minerva/persistent/users/drut1186/CCQENu_Anatuples/MuonKludge_ProtonLLR_UpdatedNeutron
