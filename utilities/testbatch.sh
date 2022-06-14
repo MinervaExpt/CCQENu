b86c52673ccfecd6cf5ca6f356c6790d393d0224
Heidi Schellman <heidi.schellman@oregonstate.edu>
2022-06-14 11:41:01 -0700

    make testbatch a bit more robust for other users

--- utilities/testbatch.sh
+++ utilities/testbatch.sh
@@ -1,10 +1,12 @@
 #export _CONDOR_SCRATCH_DIR=$PWD
 #export INPUT_TAR_DIR_LOCAL=$APP
-
+export BLUE=/minerva/data/users/$USER
+export SCRATCH=/pnfs/minerva/scratch/users/$USER
+export APP=/minerva/app/users/$USER
 # example test batch job to run on minervagpvm01 to run CCQEMAT
 # other option (not tested yet) is the EventLoop from the tutorial
 
 # type python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py to see the option descriptions
 # here my release is in $APP/NEWMAT - your mileage may differ
 
-python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/test --basedir=$APP/NEWMAT --rundir=CCQENu/make_hists --playlist=minervame5A --tag=test --mail --prescale=1000 --config=testme --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat.sh --tmpdir=$BLUE/tmp --expected-lifetime=12h --memory=2000  #--debug --notimestamp
+python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/test --basedir=$APP/NEWMAT --rundir=CCQENu/make_hists --playlist=minervame5A --tag=ccqe --mail --prescale=1 --config=hms/test_batch --exe=sidebands_v2 --sample=Background --setup=CCQENu/utilities/setup_batch_mat.sh --tmpdir=$BLUE/tmp --expected-lifetime=24h --memory=2000  #--debug --notimestamp
