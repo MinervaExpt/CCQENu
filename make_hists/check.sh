16317663ce8bcc1f4104a5191868d41ddc0d0196
Heidi Schellman <heidi.schellman@oregonstate.edu>
2022-09-18 18:23:44 -0700

    changes for tests against Amit's code

--- make_hists/check.sh
+++ make_hists/check.sh
@@ -1,4 +1,4 @@
-python /Users/schellma/Dropbox/BIGMAT/MAT/test/MnvH2D_viewer.py $1 h2D_QElike_pzmu_ptmu_bkgsub_unfold_effcorr_sigma
-python /Users/schellma/Dropbox/BIGMAT/MAT/test/MnvH1D_viewer.py $1 h_QElike_ptmu_bkgsub_unfolded_effcorr_sigma
-
+python /Users/schellma/Dropbox/BIGMAT/MAT/test/MnvH2D_viewer.py $1.root h2D_QElike_pzmu_ptmu_bkgsub_unfold_effcorr_sigma
+python /Users/schellma/Dropbox/BIGMAT/MAT/test/MnvH1D_viewer.py $1.root h_QElike_ptmu_bkgsub_unfolded_effcorr_sigma
+head $1_csvdump/*ptmu*values_1d*
 
