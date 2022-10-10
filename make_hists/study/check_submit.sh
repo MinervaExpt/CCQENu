4663e6a5f514f0f51f640cf95c3f2f9e4b4e08b5
Heidi Schellman <heidi.schellman@oregonstate.edu>
2022-10-09 19:06:45 -0700

    12hr 2000 MB

--- make_hists/study/check_submit.sh
+++ make_hists/study/check_submit.sh
@@ -7,6 +7,6 @@
 # type python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py to see the option descriptions
 # here my release is in $APP/NEWMAT - your mileage may differ
 
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/test --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --tag=check_v1 --mail --prescale=1 --config=study/CheckPRD --exe=sidebands_v2 --sample=QElike --setup=CCQENu/utilities/setup_batch_mat.sh --tmpdir=$BLUE/tmp --expected-lifetime=24h --memory=4000  #--debug --notimestamp
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/test --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --tag=check_v1 --mail --prescale=1 --config=study/CheckPRD --exe=sidebands_v2 --sample=QElike --setup=CCQENu/utilities/setup_batch_mat.sh --tmpdir=$BLUE/tmp --expected-lifetime=12h --memory=2000  #--debug --notimestamp
 
 
