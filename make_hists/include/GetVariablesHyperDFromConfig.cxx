b110de0b3d47311a01cbdc8114462928483c37ee
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2023-02-27 13:03:56 -0600

    Clean up

--- make_hists/include/GetVariablesHyperDFromConfig.cxx
+++ make_hists/include/GetVariablesHyperDFromConfig.cxx
@@ -5,13 +5,6 @@ d#include "utils/NuConfig.h"
 #include "CVUniverse.h"
 #include <cassert>
 
-std::vector<std::string> intersection(std::vector<std::string> &v1, std::vector<std::string> &v2){
-    std::vector<std::string> v3;
-    std::sort(v1.begin(), v1.end());
-    std::sort(v2.begin(), v2.end());
-    std::set_intersection(v1.begin(),v1.end(),v2.begin(),v2.end(),back_inserter(v3));
-    return v3;
-}
 
 std::map<std::string, CCQENu::VariableHyperDFromConfig*> GetHyperDVariablesFromConfig( std::vector<std::string> varsHyperD, std::map< std::string, CCQENu::VariableFromConfig* > variablesmap, const std::vector<std::string> tags, const NuConfig configraw) {
   //=========================================
@@ -29,25 +22,33 @@ std::map<std::string, CCQENu::VariableHyperDFromConfig*> GetHyperDVariablesFromC
 
   configraw.Print();
   NuConfig config;
-  if(configraw.IsMember("HyperD")){
+  if (configraw.IsMember("HyperD"))
+  {
     config = configraw.GetValue("HyperD");
-  } else{
+  } 
+  else {
     std::cout << "No HyperD variables configured." << std::endl;
+    // TODO: find way to not crash the program if these aren't configured
     exit(1);
   }
   NuConfig config1D;
-  if(configraw.IsMember("1D")){
+  if (configraw.IsMember("1D"))
+  {
     config1D = configraw.GetValue("1D");
-  } else{
+  } else
+  {
     std::cout << "No 1D variables configured." << std::endl;
   }
 
   std::vector<std::string>  keys = config.GetKeys();
-  for (auto key:keys){ // this is in the Variables json file so you can define them all
+  for (auto key:keys)
+  { // this is in the Variables json file so you can define them all
     bool found = false;
     NuConfig varconfig;
-    for ( auto v:varsHyperD){ // this is from the master driver file so you can skip some
-      if ( v == key){
+    for ( auto v:varsHyperD
+    ){ // this is from the master driver file so you can skip some
+      if ( v == key)
+      {
         found = true;
         bool foundx = false;
         bool foundy = false;
@@ -58,35 +59,41 @@ std::map<std::string, CCQENu::VariableHyperDFromConfig*> GetHyperDVariablesFromC
 
         NuConfig xvarconfig = config1D.GetValue(xvarname);
         std::vector<std::string> xfor = {};
-        if(xvarconfig.IsMember("for")){
+        if (xvarconfig.IsMember("for"))
+        {
           xfor = xvarconfig.GetStringVector("for");
         }
         NuConfig yvarconfig = config1D.GetValue(yvarname);
         std::vector<std::string> yfor = {};
-        if(yvarconfig.IsMember("for")){
+        if (yvarconfig.IsMember("for"))
+        {
           yfor = yvarconfig.GetStringVector("for");
         }
         std::vector<std::string> fors = {};
-        if(xfor.size()==0){fors = yfor;}
-        else if(yfor.size()==0){fors = xfor;}
-        else if(xfor.size()>0 && yfor.size()>0){fors = intersection(xfor, yfor);}
+        if (xfor.size()==0){fors = yfor;}
+        else if (yfor.size()==0){fors = xfor;}
+        else if (xfor.size()>0 && yfor.size()>0){fors = intersection(xfor, yfor);}
 
         CCQENu::VariableFromConfig* xvar;
         CCQENu::VariableFromConfig* yvar;
         // CCQENu::VariableHyperDFromConfig* varHyperDfromconfig;
 
-        for( auto var:variablesmap ){
+        for( auto var:variablesmap )
+        {
           std::string varname = var.first;
-          if( xvarname == varname ){
+          if ( xvarname == varname )
+          {
             xvar = variablesmap[xvarname];
             foundx = true;
           }
-          if( yvarname == varname ){
+          if ( yvarname == varname )
+          {
             yvar = variablesmap[yvarname];
             foundy = true;
           }
         }
-        if ( foundx && foundy ){
+        if ( foundx && foundy )
+        {
           // varHyperDfromconfig = new CCQENu::VariableHyperDFromConfig(nameHyperD,*xvar,*yvar,varconfig);
           // varHyperDfromconfig = new CCQENu::VariableHyperDFromConfig(nameHyperD,*xvar,*yvar,fors);
           CCQENu::VariableHyperDFromConfig* varHyperDfromconfig = new CCQENu::VariableHyperDFromConfig(nameHyperD,*xvar,*yvar,fors);
@@ -94,37 +101,41 @@ std::map<std::string, CCQENu::VariableHyperDFromConfig*> GetHyperDVariablesFromC
           std::cout << "GetVariablesHyperDFromConfig: set up HyperD variable " << nameHyperD << std::endl;
           variablesHyperDmap[nameHyperD] = varHyperDfromconfig;
         }
-        else if( !foundx ) {
+        else if ( !foundx ) 
+        {
           std::cout << " Warning - have requested an unimplemented 1D variable in GetHyperDVariablesFromConfig " << xvarname << std::endl;
-            assert(0);
+          assert(0);
         }
-        else if( !foundy ){
+        else if ( !foundy )
+        {
           std::cout << " Warning - have requested an unimplemented 1D variable in GetHyperDVariablesFromConfig " << xvarname << std::endl;
-            assert(0);
+          assert(0);
         }
       }
     }
-    if(!found){
+    if (!found)
+    {
       std::cout << "GetHyperDVariablesFromConfig: HyperD variable " << key << " configured but not requested in main config" << std::endl;
-
     }
   }
 
 // check that all requested variables are defined.
-  for (auto v:varsHyperD){
-      bool found = false;
-      for (auto key:keys){
-          if (v == key){
-              found = true;
-              break;
-          }
-        }
-
-      if (!found){
-        std::cout << "GetHyperDVariablesFromConfig: Warning - have requested an unimplemented variable in GetHyperDVariablesFromConfig " << v << std::endl;
-          assert(0);
+  for (auto v:varsHyperD)
+  {
+    bool found = false;
+    for (auto key:keys)
+    {
+      if (v == key)
+      {
+        found = true;
+        break;
       }
     }
-
+    if (!found)
+    {
+      std::cout << "GetHyperDVariablesFromConfig: Warning - have requested an unimplemented variable in GetHyperDVariablesFromConfig " << v << std::endl;
+      assert(0);
+    }
+  }
   return variablesHyperDmap;
 }
