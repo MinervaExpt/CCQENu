5645e4ce5d366d5c9001db819058650be8f558bf
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2023-04-25 16:13:46 -0500

    Set default tunedmc to untuned

--- make_hists/include/GetVariablesFromConfig.h
+++ make_hists/include/GetVariablesFromConfig.h
@@ -10,6 +10,6 @@
 //  class VariableFromConfig;
 //}
 
-std::map<std::string, CCQENu::VariableFromConfig *> GetVariablesFromConfig(const std::vector<std::string> variables, const std::vector<std::string> tags, NuConfig, const bool doresolution = false, const std::string tunedmc="both");
+std::map<std::string, CCQENu::VariableFromConfig *> GetVariablesFromConfig(const std::vector<std::string> variables, const std::vector<std::string> tags, NuConfig, const bool doresolution = false, const std::string tunedmc="untuned");
 
 #endif
