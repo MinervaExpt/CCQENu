a8dfc802d0ccfda6f59262dcbdbce52d541df7ff
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2023-05-02 23:51:22 -0500

    Making an xsec class with focus on configurability

---
+++ make_hists/include/XSec.cxx
@@ -0,0 +1,101 @@
+#ifndef XSEC_CXX
+#define XSEC_CXX
+/**
+ * @file
+ * @author  Heidi Schellman/Noah Vaughan/SeanGilligan
+ * @version 1.0 *
+ * @section LICENSE *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version. *
+ * @section DESCRIPTION *
+ * Code to fill histograms
+ */
+
+#include "include/XSec.h"
+#include "XSec.h"
+using namespace CCQENu;
+template <class MnvHistoType>
+XSec<MnvHistoType>::XSec(){}
+
+template <class MnvHistoType>
+XSec<MnvHistoType>::XSec(NuConfig* config) {}
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::SetNames(std::string basename, std::string sample, std::string variable) {
+    m_basename = basename;
+    m_sample = sample;
+    m_variable = variable;
+}
+
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::SetDataMCTruthHists(MnvHistoType* data_hist, 
+                                             MnvHistoType* mc_bkg_hist, MnvHistoType* mc_sig_hist,
+                                             MnvHistoType* sel_truth_hist, MnvHistoType* all_truth_hist) {
+    m_data_hist = data_hist.Clone();
+    m_mc_bkg_hist = mc_bkg_hist.Clone();
+    m_mc_sig_hist = mc_sig_hist.Clone();
+    m_sel_truth_hist = sel_truth_hist.Clone();
+    m_all_truth_hist = all_truth_hist.Clone();
+}
+
+template <class MnvHistoType>
+void CCQENu::XSec<MnvHistoType>::SetResponse(MnvH2D* response) {
+    m_response = response.Clone();
+}
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::SetUnfold(MinervaUnfold::MnvUnfold unfold) {
+    m_unfold = unfold;
+}
+
+template <class MnvHistoType>
+void CCQENu::XSec<MnvHistoType>::SetFluxHist(MnvH1D* flux_hist) {
+    m_flux_hist = flux_hist.Clone();
+}
+
+template <class MnvHistoType>
+void CCQENu::XSec<MnvHistoType>::SetFluxHist(MnvH2D* response) {
+}
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::MakeMCtot() {
+    std::string mc_tot_name = m_name + "_mc_tot";
+    m_mc_tot_hist = (MnvHistoType*)m_mc_sig_hist.Clone(mc_tot_name.c_str());
+    m_mc_tot_hist->SetDirectory(0);
+    m_mc_tot_hist->Add(m_mc_bkg_hist.Clone());
+    m_mc_tot_hist.SyncBands();
+}
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::SubtractBackground() {
+    std::string bkg_sub_name = m_name + "_bkgsub";
+    m_bkgsub_hist = (MnvHistoType*)m_data_hist.Clone(bkg_sub_name.cstr());
+    m_mc_tot_hist->SetDirectory(0);
+    m_bkgsubhist->Add(m_mc_bkg_hist.Clone(),-1);
+    m_bkgsubhist.SyncBands();
+}
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::DoUnfolding() {
+}
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::DoEfficiencyCorrection() {
+}
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::FluxNormalize() {
+}
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::TargetNormalize() {
+}
+
+template <class MnvHistoType>
+void XSec<MnvHistoType>::POTNormalize() {
+}
+
+#endif // XSEC_CXX
---
+++ make_hists/include/XSec.h
@@ -0,0 +1,128 @@
+#ifndef XSEC_H
+#define XSEC_H
+/**
+ * @file
+ * @author  Heidi Schellman/Noah Vaughan/SeanGilligan
+ * @version 1.0 *
+ * @section LICENSE *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version. *
+ * @section DESCRIPTION *
+ * Code to fill histograms
+ */
+
+#include <iostream>
+#include <string>
+
+#include "MinervaUnfold/MnvResponse.h"
+#include "MinervaUnfold/MnvUnfold.h"
+#include "PlotUtils/FluxReweighter.h"
+#include "PlotUtils/MnvH1D.h"
+#include "PlotUtils/MnvH2D.h"
+#include "TCanvas.h"
+#include "TF2.h"
+#include "TH2D.h"
+#include "utils/NuConfig.h"
+#include "utils/RebinFlux.h"
+#include "utils/SyncBands.h"
+
+#ifndef __CINT__
+#include "include/plotting_pdf.h"
+#endif
+
+/*
+================================= GetXSec.h ====================================
+
+This templates the analysis steps of doing a 1D and 2D cross section analysis at
+MINERvA. Each step is templated to recieve both 1D and 2D inputs. The steps are
+as follows:
+  - MakeMC: Combine MC signal and bkg to make full MC
+  - GetSignalFraction: Uses MC bkg and full MC to create a signal fraction
+  - DoBkgSutraction: Uses signal fraction to make bkg on data, then subtracts it
+      from data
+  - DoUnfolding: Does unfolding in one oftwo ways depending on the variable you
+      are analyzing: 1. Crude ratio of (selected truth)/(all truth), 2. Using
+      UnfoldUtils.
+  - DoEfficiencyCorrection: Corrects unsmeared data for efficiency.
+  - DoNormalization: Normalizes for Flux and number of targets. For 1D can do an
+      flat flux or energy dependent flux if the variable needs it. For 2D can do
+      only flat right now.
+
+
+Noah Harvey Vaughan, May 2021
+Schellman Neutrino Group, Oregon State University
+vaughann@oregonstate.edu
+================================================================================
+*/
+//============================ Some Useful Tools ===============================
+namespace CCQENu {
+template <class MnvHistoType>
+class XSec {
+   public:
+    XSec();
+    XSec(NuConfig* config);
+
+    std::string m_name;
+    std::string m_base_name;
+    std::string m_variable_name;
+    std::string m_sample_name;
+
+    NuConfig* m_main_config;
+    std::string m_tunedmc;
+
+   private:
+    MnvHistoType* m_data_hist;
+    MnvHistoType* m_bkgsub_hist;
+    MnvHistoType* m_unfolded_hist;
+    MnvHistoType* m_effcorr_hist;
+    MnvHistoType* m_sigma_hist;
+
+    MnvHistoType* m_mc_sig_hist;
+    MnvHistoType* m_mc_bkg_hist;
+    MnvHistoType* m_mc_tot_hist;
+
+    MnvHistoType* m_sel_truth_hist;
+    MnvHistoType* m_all_truth_hist;
+    MnvHistoType* m_sigma_mc;
+
+    MnvH2D* m_response;
+    MinervaUnfold::MnvUnfold m_unfold;
+
+
+    MnvH1D* m_flux_hist;
+    MnvHistoType* m_flux_ebins;
+    MnvHistoType* m_flux_varbins;
+
+   public:
+    TCanvas& m_canvas;
+
+    void SetNames(std::string basename, std::string sample, std::string variable);
+
+    
+    void SetDataMCTruthHists(MnvHistoType* data_hist, 
+                             MnvHistoType* mc_bkg_hist, MnvHistoType* mc_sig_hist,
+                             MnvHistoType* sel_truth_hist, MnvHistoType* all_truth_hist);
+
+
+    void SetResponse(MnvH2D* response);
+    void SetUnfold(MinervaUnfold::MnvUnfold unfold);
+
+    void SetFluxHist(MnvH1D* flux_hist)
+
+    void MakeMCtot();
+    void SubtractBackground();
+    void DoUnfolding();
+    void DoEfficiencyCorrection();
+    void FluxNormalize();
+    void TargetNormalize();
+    void POTNormalize();
+    // Helpers
+    std::vector<std::string> split(std::string s, std::string delimiter);
+    TMatrixD ZeroDiagonal(const TMatrixD& m);
+    std::map<std::string, bool> CheckFluxNorm(NuConfig* configvar, std::string variable);
+}  // class CrossSection
+}  // namespace CCQENu
+
+#endif //XSEC_H
