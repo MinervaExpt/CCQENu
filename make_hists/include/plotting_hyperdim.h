f9c4c8dd6ca05e48628d506d05dabe12306cb74d
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2023-05-19 16:30:01 -0500

    First commit for hyperd plotting code.

---
+++ make_hists/include/plotting_hyperdim.h
@@ -0,0 +1,15 @@
+#ifndef PLOTTING_HYPERDIM_H
+#define PLOTTING_HYPERDIM_H
+#include <iostream>
+
+#include "PlotUtils/HistogramUtils.h"
+#include "PlotUtils/MnvH1D.h"
+#include "PlotUtils/MnvH2D.h"
+#include "PlotUtils/MnvPlotter.h"
+#include "PlotUtils/MnvVertErrorBand.h"
+#include "TCanvas.h"
+#include "TText.h"
+
+void 
+
+#endif  // PLOTTING_HYPERDIM_H
