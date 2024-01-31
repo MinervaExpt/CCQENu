a4cad90b861a480d91e51707ecb354094bff2665
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2024-01-30 22:00:12 -0800

    cleanup etc

--- make_hists/include/Get2DVariablesFromConfig.h
+++ make_hists/include/Get2DVariablesFromConfig.h
@@ -1,18 +1,17 @@
 #ifndef GET2DVariablesFromConfig_H
 #define GET2DVariablesFromConfig_H
-#include <vector>
-#include <string>
 #include <map>
+#include <string>
+#include <vector>
 
 #include "CVUniverse.h"
 #include "PlotUtils/PhysicsVariables.h"
 
-namespace CCQENu
-{
-  class VariableFromConfig;
-  class Variable2DFromConfig;
-}
-std::vector<std::string> intersection(std::vector<std::string> &v1, std::vector<std::string> &v2); // tool used to find "for" for x and y
+namespace CCQENu {
+class VariableFromConfig;
+class Variable2DFromConfig;
+}  // namespace CCQENu
+std::vector<std::string> intersection(std::vector<std::string> &v1, std::vector<std::string> &v2);  // tool used to find "for" for x and y
 std::map<std::string, CCQENu::Variable2DFromConfig *> Get2DVariablesFromConfig(std::vector<std::string> vars2D, std::map<std::string, CCQENu::VariableFromConfig *> variablesmap, const std::vector<std::string> tags, const NuConfig configraw);
 
 #endif
