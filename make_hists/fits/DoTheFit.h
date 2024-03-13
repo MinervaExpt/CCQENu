4f9c9c844c0833c8b285c20071d3f68f544050e8
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2024-03-12 21:36:24 -0700

    bug: formatting

--- make_hists/fits/DoTheFit.h
+++ make_hists/fits/DoTheFit.h
@@ -1,12 +1,20 @@
 #ifndef DOTHEFIT_h
 #define DOTHEFIT_h
-#include "PlotUtils/MnvH1D.h"
-#include "fits/MultiScaleFactors.h"
 #include <map>
 #include <vector>
 
-namespace fit{
+#include "PlotUtils/MnvH1D.h"
+#include "fits/MultiScaleFactors.h"
 
-int DoTheFit(std::map<const std::string, std::vector< PlotUtils::MnvH1D*>> fitHists, const std::map<const std::string, std::vector< PlotUtils::MnvH1D*> > unfitHists, const std::map<const std::string, PlotUtils::MnvH1D*>  dataHist, const std::map<const std::string, bool> includeInFit, const std::vector<std::string> categories, const fit_type type = kML, const int lowBin = 1, const int hiBin = -1, const bool binbybin=false);
+namespace fit {
+
+int DoTheFit(std::map<const std::string, std::vector<PlotUtils::MnvH1D*>> fitHists,
+             const std::map<const std::string, std::vector<PlotUtils::MnvH1D*>> unfitHists,
+             const std::map<const std::string, PlotUtils::MnvH1D*> dataHist,
+             const std::map<const std::string, bool> includeInFit,
+             const std::vector<std::string> categories,
+             const fit_type type = kML,
+             const int lowBin = 1, const int hiBin = -1, 
+             const bool binbybin = false);
 };
 #endif
