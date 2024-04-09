0c3ce5cfb56ed3092d6829056d5568b2655c76af
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2024-04-08 19:46:31 -0700

    more consistency

--- utilities/test9.sh
+++ utilities/test9.sh
@@ -8,6 +8,6 @@
 # here my release is in $APP/NEWMAT - your mileage may differ
 
 python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/test \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=MnvTunev2 --tag=example_v2 \
- --mail --prescale=100 --config=Test --exe=sidebands_v2 --sample=QElike --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=MnvTunev2 --tag=SL9 \
+ --mail --prescale=100 --config=Mar24 --exe=sidebands_v2 --sample=QElike --setup=CCQENu/utilities/setup_batch_mat9.sh \
  --tmpdir=$BLUE/tmp --expected-lifetime=4h --memory=3000   #--debug --notimestamp
