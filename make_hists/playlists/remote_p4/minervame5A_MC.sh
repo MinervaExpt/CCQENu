9a25d274ca82010c9c2a90cf92b7cb8e226a8eab
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2024-04-13 13:43:19 -0700

    add p4 playlists

---
+++ make_hists/playlists/remote_p4/minervame5A_MC.sh
@@ -0,0 +1,24 @@
+export MINERVA=minervagpvm02.fnal.gov
+export DATA=$HOME/data/p4
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123000_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123001_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123002_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123003_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123004_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123005_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123006_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123007_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123008_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123009_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123010_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123011_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123012_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123013_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123014_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123015_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123016_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123017_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123018_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123019_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123020_Playlist.root $DATA/.
+scp $MINERVA:/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p4/FullDetector/Merged_mc_ana_me5A_DualVertex_p4/MasterAnaDev_mc_AnaTuple_run00123021_Playlist.root $DATA/.
