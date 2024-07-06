b39dbdfe0c04c3e4a69a7c3b20fc7362c5c955a8
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2024-07-05 18:47:30 -0700

    full sub playlist for hep01

---
+++ make_hists/nhv/config/hep01/submitfull.sh
@@ -0,0 +1,22 @@
+export MYPLAYLIST=minervame5A
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+# export MYPLAYLIST=minervame6A
+# ./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+export MYSAMPLE=minervame6B
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+export MYPLAYLIST=minervame6C
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+export MYPLAYLIST=minervame6D
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+export MYPLAYLIST=minervame6E
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+export MYPLAYLIST=minervame6F
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+export MYPLAYLIST=minervame6G
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+export MYPLAYLIST=minervame6H
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+export MYPLAYLIST=minervame6I
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
+export MYPLAYLIST=minervame6J
+./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
