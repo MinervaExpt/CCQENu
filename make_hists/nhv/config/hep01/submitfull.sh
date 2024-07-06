a712d85a6ac6fa33e4d6d36237a8ba622c43f017
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2024-07-05 20:04:48 -0700

    bug on 6B in submit script

--- make_hists/nhv/config/hep01/submitfull.sh
+++ make_hists/nhv/config/hep01/submitfull.sh
@@ -2,7 +2,7 @@ export MYPLAYLIST=minervame5A
 ./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
 export MYPLAYLIST=minervame6A
 ./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
-export MYSAMPLE=minervame6B
+export MYPLAYLIST=minervame6B
 ./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
 export MYPLAYLIST=minervame6C
 ./sidebands_v2 nhv/config/hep01/AntiNu_v15_SBStudy 1
