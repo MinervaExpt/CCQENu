d72d52612cdb2f8c3c07d5966ac28a594b27f3b2
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2024-07-07 22:16:51 -0700

    change playlist

--- utilities/sbfits_07062024.sh
+++ utilities/sbfits_07062024.sh
@@ -8,6 +8,6 @@
 # here my release is in $APP/NEWMAT - your mileage may differ
 
 python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer24Collab/fits \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6F --model=MnvTunev1 --tag=AL9 \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=MnvTunev1 --tag=AL9 \
  --mail --prescale=1 --config=nhv/config/Summer24Collab/AntiNu_v15_SBStudy_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
  --tmpdir=$SCRATCH/tmp --expected-lifetime=24h --memory=4000   #--debug --notimestamp
