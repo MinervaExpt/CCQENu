ae8a93e844175522c5e6be3a2d4124bcc1b9432c
Noah Harvey Vaughan <22598881+nhvaughan@users.noreply.github.com>
2024-07-08 00:17:16 -0700

    loosen blob cut

--- utilities/sbfits_07062024.sh
+++ utilities/sbfits_07062024.sh
@@ -7,7 +7,7 @@
 # type python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py to see the option descriptions
 # here my release is in $APP/NEWMAT - your mileage may differ
 
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer24Collab/fits \
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer24Collab/fits/openblobsb \
  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=MnvTunev1 --tag=AL9 \
  --mail --prescale=1 --config=nhv/config/Summer24Collab/AntiNu_v15_SBStudy_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
  --tmpdir=$SCRATCH/tmp --expected-lifetime=24h --memory=4000   #--debug --notimestamp
