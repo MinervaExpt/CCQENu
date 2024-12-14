f4d9f085ac8b5edc421338e320b299e677bb6c65
Heidi Schellman <heidi.schellman@oregonstate.edu>
2024-12-14 13:13:02 -0800

    try to find stuff in new rootless conda stack

--- make_hists/setup_osx.sh
+++ make_hists/setup_osx.sh
@@ -9,9 +9,9 @@ source $WHEREIPUTMYCODE/opt/build/setup.sh     # you have to have this
 #2export JSONCPP_DIR=$HOME/LocalApps/jsoncpp-build # need to change this probably
 
 export CCQEMAT=$WHEREIPUTMYCODE/CCQENu/make_hists
-
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$ROOTSYS/lib
 export DYLD_LIBRARY_PATH=${PLOTUTILSROOT}:${JSONCPP_DIR}/lib
-export PYTHONPATH=$CCQEMAT/python:$WHEREIPUTMYCODE/MAT-MINERvA/python:$WHEREIPUTMYCODE/MAT-MINERvA/python/PlotUtils:$PYTHONPATH
+export PYTHONPATH=$CCQEMAT/python:$WHEREIPUTMYCODE/MAT-MINERvA/python:$WHEREIPUTMYCODE/MAT-MINERvA/python/PlotUtils:$WHEREIPUTMYCODE/UnfoldUtils/python/MinervaUnfold:$WHEREIPUTMYCODE/UnfoldUtils/python/UnfoldUtils:$PYTHONPATH
 export THEDATA=/pnfs/minerva/persistent/users/drut1186/CCQENu_Anatuples/MuonKludge_ProtonLLR_UpdatedNeutron
 
 sed s+GIT_COMMIT_HASH+`git rev-parse --verify HEAD`+ utils/gitVersion.h.in > utils/gitVersion.h
