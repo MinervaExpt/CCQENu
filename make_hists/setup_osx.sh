98e63a03a74aff599fd7d538ae906b9a79c17fe3
Heidi Schellman <heidi.schellman@oregonstate.edu>
2024-12-14 11:42:05 -0800

    hacked version with json.h as had to build root standalone

--- make_hists/setup_osx.sh
+++ make_hists/setup_osx.sh
@@ -6,11 +6,11 @@ export CVSROOT=minervacvs@cdcvs.fnal.gov:/cvs/mnvsoft
 #export WHEREIPUTMYCODE=$HOME/Dropbox/BIGMAT    # change this here if you want a different location
 source $WHEREIPUTMYCODE/opt/build/setup.sh     # you have to have this
 
-export JSONCPP_DIR=$HOME/LocalApps/jsoncpp-build # need to change this probably
+#2export JSONCPP_DIR=$HOME/LocalApps/jsoncpp-build # need to change this probably
 
 export CCQEMAT=$WHEREIPUTMYCODE/CCQENu/make_hists
 
-export DYLD_LIBRARY_PATH=${PLOTUTILSROOT}
+export DYLD_LIBRARY_PATH=${PLOTUTILSROOT}:${JSONCPP_DIR}/lib
 export PYTHONPATH=$CCQEMAT/python:$WHEREIPUTMYCODE/MAT-MINERvA/python:$WHEREIPUTMYCODE/MAT-MINERvA/python/PlotUtils:$PYTHONPATH
 export THEDATA=/pnfs/minerva/persistent/users/drut1186/CCQENu_Anatuples/MuonKludge_ProtonLLR_UpdatedNeutron
 
