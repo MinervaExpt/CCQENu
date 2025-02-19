c5e8e88c6952af16c1787d8b93789290b3f8905a
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2025-02-18 22:44:12 -0800

    bug: commas on the bins lists

--- GENIE3/genie3_qelike_hyperon_study.py
+++ GENIE3/genie3_qelike_hyperon_study.py
@@ -1,5 +1,6 @@
 import ROOT
 import array
+import numpy as np
 import sys
 
 # Stolen from Amit's area. Used to do extraction of cross sections from raw GENIE v3 files for the purpose of comaprison.
@@ -227,8 +228,8 @@ mytree = inputFile.Get("FlatTree_VARS")
 ofilename = sys.argv[2]
 
 # Bins from 3d analysis (might change)
-pzbins = [1.5, 2., 2.5, 3., 3.5, 4., 4.5, 5., 5.5, 6., 7., 8., 9., 10., 15.],
-ptbins = [0., 0.075, 0.15, 0.25, 0.325, 0.4, 0.475, 0.55, 0.7, 0.85, 1., 1.25, 1.5, 2.5],
+pzbins = [1.5, 2., 2.5, 3., 3.5, 4., 4.5, 5., 5.5, 6., 7., 8., 9., 10., 15.]
+ptbins = [0., 0.075, 0.15, 0.25, 0.325, 0.4, 0.475, 0.55, 0.7, 0.85, 1., 1.25, 1.5, 2.5]
 recoilbins = [0.0, 0.05, 0.10, 0.15, 0.30, 0.50, 1.0]
 
 # myptpz = ROOT.TH2D("ptpz","ptpz",len(pzbins)-1, array.array("d",pzbins),len(ptbins)-1,array.array("d",ptbins))
