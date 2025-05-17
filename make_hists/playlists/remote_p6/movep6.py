280696e00c575a7129ce9f4533f40350b1a182bd
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-05-17 12:40:03 -0700

    autogenerate new playlists

--- make_hists/playlists/remote_p6/movep6.py
+++ make_hists/playlists/remote_p6/movep6.py
@@ -6,13 +6,17 @@ if "MC" in sys.argv[1]:
     newpath = "/minerva/data/ME/p6/FullDetector/"
 f = open(sys.argv[1],'r')
 g = open(sys.argv[1].replace(".txt",".sh"),'w')
+p = open("../hep01_p6/"+sys.argv[1],'w')
 log = sys.argv[1].replace(".txt",".log")
 files = f.readlines()
-g.write("touch",newloc,"\n")
+g.write("touch "+log + "\n")
 for file in files:
     newloc = file.replace(oldpath,newpath).strip()
     newline = file.replace("root:","xrdcopy root:")
     newline = newline.replace(".root",".root "+newloc+ " >>& " + log+"\n" )
-    
+    p.write(newloc+"\n")
     g.write(newline)
     print (newline)
+
+g.close()
+p.close()
