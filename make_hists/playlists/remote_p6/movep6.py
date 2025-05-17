46eb1281565092ea41fc230eedf1739f8ed58ef1
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-05-17 10:39:52 -0700

    make it do a log

--- make_hists/playlists/remote_p6/movep6.py
+++ make_hists/playlists/remote_p6/movep6.py
@@ -3,9 +3,10 @@ oldpath = "root://fndca1.fnal.gov:1094/pnfs/fnal.gov/usr/minerva/persistent/Data
 newpath = "/minerva/data/ME/p6/p6PrimeData/"
 if "MC" in sys.argv[1]:
     oldpath="root://fndca1.fnal.gov:1094/pnfs/fnal.gov/usr/minerva/persistent/DataPreservation/p6/FullDetector/"
+    newpath = "/minerva/data/ME/p6/FullDetector/"
 f = open(sys.argv[1],'r')
 g = open(sys.argv[1].replace(".txt",".sh"),'w')
-log = replace(".txt",".log")
+log = sys.argv[1].replace(".txt",".log")
 files = f.readlines()
 for file in files:
     newloc = file.replace(oldpath,newpath)
