94c725f100bf9880c4e451eddf2dd6a972bd150a
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2025-06-20 16:00:57 -0700

    updates and add ratio

--- python/compareHist.py
+++ python/compareHist.py
@@ -6,7 +6,7 @@ from ROOT import gStyle
 from ROOT import SetOwnership
 
 from PlotUtils import MnvH1D
-DEBUG = False
+DEBUG = True
 
 filter = ["response","types","resolution"]
 
@@ -17,6 +17,13 @@ def CCQELegend(xlow,ylow,xhigh,yhigh):
   leg.SetBorderSize(0)
   leg.SetTextSize(0.03)
   return leg
+
+def CCQECanvas(name,title,xsize=750,ysize=750):
+    c2 = TCanvas(name,title,xsize,ysize)
+    c2.SetLeftMargin(0.20)
+    c2.SetRightMargin(0.05)
+    c2.SetBottomMargin(0.15)
+    return c2
 
 def text(canvas,h1):
     if not h1.GetTitle(): 
@@ -39,8 +46,8 @@ def loop(outname,canvas,fname,gname):
     gpot = g.Get("POT_summary")
     pot2 = gpot.GetBinContent(1)
     if pot1 > 0: 
-        scaleData = pot2/pot1
-        scaleMC = gpot.GetBinContent(3)/fpot.GetBinContent(3)
+        scaleData = pot1/pot2
+        scaleMC = fpot.GetBinContent(3)/gpot.GetBinContent(3)
     print ("POT",pot1,pot2,scaleData,scaleMC)
     
     for k in f.GetListOfKeys():
@@ -51,7 +58,7 @@ def loop(outname,canvas,fname,gname):
             if fil in key:
                 badkey = True
         if badkey: continue
-        canvas = TCanvas(key,key)
+        canvas = CCQECanvas(key,key)
         h1 = MnvH1D()
         h2 = MnvH1D()
         h1 = f.Get(key)
@@ -89,10 +96,16 @@ def loop(outname,canvas,fname,gname):
         
 
 def compare(outname,canvas, h1, h2, key1, key2):
-    #canvas.Divide(2)
-    canvas.cd(1)
+    canvas.Divide(1,2,0.01,0.01)
+    canvas.cd()
+    #pad1 =  TPad("COMP","COMP",0,.5,1,1)
+    #pad1.cd()
     #text(canvas,h1)
     #canvas.SetLogy()
+    canvas.cd(1)
+    gPad.SetTopMargin(0.1)
+    gPad.SetBottomMargin(0.2)
+    gPad.SetLeftMargin(0.2)
     nbins = h1.GetXaxis().GetNbins()
     res = np.zeros(nbins+1)
     x = np.zeros(nbins+1)
@@ -100,17 +113,29 @@ def compare(outname,canvas, h1, h2, key1, key2):
     chi2 = h1.Chi2Test(h2,"WW P CHI2",res)
     for i in range(1,nbins+1):
         x[i-1]= h1.GetXaxis().GetBinCenter(i)
-        res[i-1] = h1.GetBinContent(i)
+        #res[i-1] = h1.GetBinContent(i)
     
     #if chi2 < 0.2: 
     #    return
-    leg = CCQELegend(0.65,0.55,0.95,0.75)
-    #leg = TLegend(0.1,0.7,0.48,0.9)
+    leg = CCQELegend(0.65,0.40,1,0.60)
+    leg.SetTextSize(0.06)
+    #leg = TLegend(0.1,0.,0.48,0.9)
+    h1.GetXaxis().SetLabelSize(0.06)
+    h1.GetYaxis().SetLabelSize(0.06)
+    h1.GetXaxis().SetTitleSize(0.06)
+    h1.GetYaxis().SetTitleSize(0.06)
+    h1.GetYaxis().SetTitle("events/unit")
+    h1.GetXaxis().CenterTitle(True)
+    h1.GetYaxis().CenterTitle(True)
     h1.SetMarkerColor(4)
     h1.SetMarkerStyle(20)
     h1.Draw("PE")
     h2.SetMarkerColor(2)
     h2.SetMarkerStyle(21)
+    h2.GetXaxis().SetLabelSize(0.1)
+    h2.GetYaxis().SetLabelSize(0.1)
+    h2.GetXaxis().SetTitleSize(0.1)
+    h2.GetYaxis().SetTitleSize(0.1)
     h2.Draw("PE same")
     print ("keys",key1,key2)
     leg.SetHeader("Version")
@@ -119,28 +144,52 @@ def compare(outname,canvas, h1, h2, key1, key2):
     leg.Print("ALL")
     leg.Draw()
     #canvas.Modified()
-    canvas.Update()
+    #canvas.Update()
     if DEBUG:
         print ("x",x)
         print ("res",res)
-    # canvas.cd(2)
-    # resgr = TGraph(nbins,x,res)
-    # resgr.GetYaxis().SetTitle("Normalized Residuals")
-    # resgr.GetXaxis().SetTitle(h1.GetTitle())
-    # resgr.SetMarkerStyle(21)
-    # resgr.SetMarkerColor(1)
-    # resgr.SetMarkerSize(.9)
-    # #resgr.SetTitle("Normalized Residuals")
-    # if DEBUG:
-    #     resgr.Print("ALL")
-    # resgr.Draw("L")
-    #gPad.Update()
-   
-    #resgr.Print()
-    canvas.Modified()
+    #pad1.cd()
+    #pad1.Draw()
     canvas.Update()
+    #pad2 =  TPad("pad2","pad2",0,.3,1,.4)
+    #pad2.cd()
+    canvas.cd(2)
+    gPad.SetTopMargin(0.1)
+    gPad.SetBottomMargin(0.2)
+    gPad.SetLeftMargin(0.2)
+    #gPad.SetPad(0,0,1,.4)
+    resgr = TGraph(nbins,x,res)
+    SetOwnership( resgr, 1 )
+    resgr.GetYaxis().SetTitle("Normalized Residuals")
+    resgr.GetXaxis().SetTitle(h1.GetTitle())
+    resgr.SetMarkerStyle(21)
+    resgr.SetMarkerColor(1)
+    resgr.SetMarkerSize(.9)
+    #resgr.SetTitle("Normalized Residuals")
+    if DEBUG:
+        resgr.Print("ALL")
+    #resgr.Draw("PE")
+    #h1.Draw("")
+    frame = MnvH1D()
+    frame = h1.Clone("ratio")
+    frame.GetYaxis().SetTitle("%s/%s"%(key1,key2))
+    frame.Divide(frame,h2,1.,1.,"B")
+    frame.SetMaximum(1.05)
+    frame.SetMinimum(0.95)
+    frame.GetXaxis().CenterTitle(True)
+    frame.GetYaxis().CenterTitle(True)
+    frame.Draw()
+    #resgr.GetXaxis().SetRangeUser(4,16)
+    #resgr.GetYaxis().SetRangeUser(-3.5,3.5)
+    gPad.SetGridy()
+    resgr.Draw("same")
+    #Pad.Update()
+    #pad2.Draw()
+    #resgr.Print()
+    #canvas.Modified()
+    #canvas.Update()
     canvas.cd()  
-    pad5 =  TPad("all","all",0,0,1,1)
+    pad5 =  TPad("all","all",0,.5,1,1)
     pad5.SetFillStyle(4000)  # transparent
     pad5.Draw()
     pad5.cd()
@@ -148,7 +197,7 @@ def compare(outname,canvas, h1, h2, key1, key2):
     #lat.DrawLatexNDC(.4,.95,"My Title")
     
     label = h1.GetName().replace("___",", ")
-    label2 = "KSprob = %6.4f"%(kol)
+    label2 = "#bf{#it{KSprob = %6.4f}}"%(kol)
    
     lat.DrawLatexNDC(.1,.95,label)
     lat.DrawLatexNDC(.6,.8,label2)
@@ -173,10 +222,7 @@ def compare(outname,canvas, h1, h2, key1, key2):
 def startPDF(filename,canvas1D):
     ''' start a pdf '''
     pdfstart1D = filename + "_1D.pdf("
-    canvas1D.SetLeftMargin(0.15)
-    canvas1D.SetRightMargin(0.15)
-    canvas1D.SetBottomMargin(0.15)
-    canvas1D.SetTopMargin(0.25)
+    
     canvas1D.Print(pdfstart1D, "pdf")
 
 def newPage(filename,canvas1D):
@@ -219,7 +265,7 @@ h2.Scale(scale,"width")
 if DEBUG:
     h1.Print()
     h2.Print()
-c1 = TCanvas("c1","c1")
+c1 = CCQECanvas("c1","c1")
 outname=os.path.basename(fname.replace(".root",""))
 startPDF(outname,c1)
 compare(outname,c1,h1,h2,"a","b")
@@ -227,7 +273,7 @@ endPDF(outname,c1)
 c1.Print("pix/test.png")
 f.Close()
 g.Close()
-c2 = TCanvas("c2","c2")
+c2 = CCQECanvas("c2","c2")
 startPDF("a_"+outname,c2)
 loop("a_"+outname,c2,fname,gname)
 endPDF("a_"+outname,c2)
