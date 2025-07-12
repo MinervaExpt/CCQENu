6cc7d502e7c2793c3098f84328baecf2a06b51bc
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2025-07-11 19:43:58 -0700

    6J Submission

--- utilities/bigrun_warpingstudies_p6.sh
+++ utilities/bigrun_warpingstudies_p6.sh
@@ -737,105 +737,13 @@
 
 
 
-# QElike
-export MYWARP=none
-export MYSAMPLE=QElike
-export MYMODEL=MnvTunev1
-
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
-
-export MYWARP=no2p2htune
-# export MYSAMPLE=QElike
-export MYMODEL=MnvTunev1
-
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
-
-export MYWARP=none
-# export MYSAMPLE=QElike
-export MYMODEL=MnvTunev2
-
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
-
-export MYSAMPLE=QElike_maxrecoil
-export MYWARP=none
-export MYMODEL=MnvTunev1
-
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
-
-export MYWARP=no2p2htune
-export MYMODEL=MnvTunev1
-
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
-
-export MYWARP=none
-export MYMODEL=MnvTunev2
-
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
-
-export MYSAMPLE=QElike_maxrecoil_hiEAvail
-export MYWARP=none
-export MYMODEL=MnvTunev1
-
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
-
-export MYWARP=no2p2htune
-export MYMODEL=MnvTunev1
-
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
-
-export MYWARP=none
-export MYMODEL=MnvTunev2
-
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
- --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
-#export _CONDOR_SCRATCH_DIR=$PWD
-#export INPUT_TAR_DIR_LOCAL=$APP
-
-# example test batch job to run on minervagpvm01 to run CCQEMAT
-# other option (not tested yet) is the EventLoop from the tutorial
-
-# type python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py to see the option descriptions
-# here my release is in $APP/NEWMAT - your mileage may differ
-
-
-
-
-
-
-
 # # QElike
 # export MYWARP=none
 # export MYSAMPLE=QElike
 # export MYMODEL=MnvTunev1
 
 # python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
-#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
 #  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
 #  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
 
@@ -844,7 +752,7 @@ python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT
 # export MYMODEL=MnvTunev1
 
 # python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
-#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
 #  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
 #  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
 
@@ -853,7 +761,7 @@ python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT
 # export MYMODEL=MnvTunev2
 
 # python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
-#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
 #  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
 #  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
 
@@ -862,7 +770,7 @@ python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT
 # export MYMODEL=MnvTunev1
 
 # python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
-#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
 #  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
 #  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
 
@@ -870,7 +778,7 @@ python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT
 # export MYMODEL=MnvTunev1
 
 # python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
-#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
 #  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
 #  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
 
@@ -878,7 +786,7 @@ python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT
 # export MYMODEL=MnvTunev2
 
 # python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
-#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
 #  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
 #  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
 
@@ -887,7 +795,7 @@ python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT
 # export MYMODEL=MnvTunev1
 
 # python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
-#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
 #  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
 #  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
 
@@ -895,7 +803,7 @@ python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT
 # export MYMODEL=MnvTunev1
 
 # python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
-#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
 #  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
 #  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
 
@@ -903,6 +811,98 @@ python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT
 # export MYMODEL=MnvTunev2
 
 # python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
-#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+#  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6I --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
 #  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
 #  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
+# #export _CONDOR_SCRATCH_DIR=$PWD
+# #export INPUT_TAR_DIR_LOCAL=$APP
+
+# # example test batch job to run on minervagpvm01 to run CCQEMAT
+# # other option (not tested yet) is the EventLoop from the tutorial
+
+# # type python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py to see the option descriptions
+# # here my release is in $APP/NEWMAT - your mileage may differ
+
+
+
+
+
+
+
+# QElike
+export MYWARP=none
+export MYSAMPLE=QElike
+export MYMODEL=MnvTunev1
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
+
+export MYWARP=no2p2htune
+# export MYSAMPLE=QElike
+export MYMODEL=MnvTunev1
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
+
+export MYWARP=none
+# export MYSAMPLE=QElike
+export MYMODEL=MnvTunev2
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
+
+export MYSAMPLE=QElike_maxrecoil
+export MYWARP=none
+export MYMODEL=MnvTunev1
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
+
+export MYWARP=no2p2htune
+export MYMODEL=MnvTunev1
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
+
+export MYWARP=none
+export MYMODEL=MnvTunev2
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
+
+export MYSAMPLE=QElike_maxrecoil_hiEAvail
+export MYWARP=none
+export MYMODEL=MnvTunev1
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
+
+export MYWARP=no2p2htune
+export MYMODEL=MnvTunev1
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
+
+export MYWARP=none
+export MYMODEL=MnvTunev2
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/Summer25Collab/eventloopout/recoilstudy \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame6J --model=${MYMODEL} --warp=${MYWARP} --tag=EAvailWarpFill_recoilstudy_${MYSAMPLE} \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
