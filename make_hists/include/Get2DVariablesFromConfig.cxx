1c4c8d8e992a22dc4a23aa00a4ff55a5b2ac2470
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2025-09-18 21:41:29 -0700

    higher dim variables can build by name only in main config

--- make_hists/include/Get2DVariablesFromConfig.cxx
+++ make_hists/include/Get2DVariablesFromConfig.cxx
@@ -8,6 +8,21 @@ std::vector<std::string> intersection(std::vector<std::string> &v1, std::vector<
     return v3;
 }
 
+std::vector<std::string> split2D(std::string s, std::string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::string token;
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
+
 std::map<std::string, CCQENu::Variable2DFromConfig *> Get2DVariablesFromConfig(std::vector<std::string> vars2D, std::map<std::string, CCQENu::VariableFromConfig *> variablesmap, const std::vector<std::string> tags, const NuConfig configraw) {
     //=========================================
     // Constructor wants: name,
@@ -126,77 +141,97 @@ std::map<std::string, CCQENu::Variable2DFromConfig *> Get2DVariablesFromConfig(s
 
     // configraw.Print();
     NuConfig config2D;
+    bool _found_config2D = false;
     if (configraw.IsMember("2D")) {
         config2D = configraw.GetValue("2D");
+        bool _found_config2D = true;
     } else {
-        std::cout << "No 2D variables configured." << std::endl;
-        exit(1);
+        // std::cout << "ERROR: No 2D variables configured." << std::endl;
+        // exit(1);
+        std::cout << "WARNING: No 2D variables configured. Will try to build from 1D vars..." << std::endl;
     }
     NuConfig config1D;
     if (configraw.IsMember("1D")) {
         config1D = configraw.GetValue("1D");
     } else {
-        std::cout << "No 1D variables configured." << std::endl;
+        std::cout << "ERROR: No 1D variables configured." << std::endl;
+        exit(1);
     }
     std::vector<std::string> keys = config2D.GetKeys();
     for (auto v : vars2D) {
-        NuConfig tmp_varconfig;
         // CCQENu::VariableFromConfig *xvar;
         // CCQENu::VariableFromConfig *yvar;
+        std::string name2D = "";
+        std::string xvar_name = "";
+        std::string yvar_name = "";
+        std::vector<std::string> fors = {};
 
         if (config2D.IsMember(v)) {
             NuConfig varconfig = config2D.GetValue(v);
-            std::string name2D = varconfig.GetString("name");
-            std::string xvar_name = varconfig.GetString("xvar");
-            std::string yvar_name = varconfig.GetString("yvar");
-            if (!config1D.IsMember(xvar_name)){
-                std::cout << "Get2DVariablesFromConfig: ERROR - have requested an unimplemented 1D variable in Get2DVariablesFromConfig " << xvar_name << std::endl;
-                assert(0);
-            }
-            if (!config1D.IsMember(yvar_name)) {
-                std::cout << "Get2DVariablesFromConfig: ERROR - have requested an unimplemented 1D variable in Get2DVariablesFromConfig " << yvar_name << std::endl;
-                assert(0);
-            }
-            // CCQENu::VariableFromConfig *xvar = new CCQENu::VariableFromConfig(config1D.GetValue(xvar_name));
-            CCQENu::VariableFromConfig *xvar = new CCQENu::VariableFromConfig(config1D.GetValue(xvar_name));
-            xvar->SetDoResolution(doresolution);
-            xvar->SetDoTypes(dotypes);
-            xvar->SetTunedMC(tunedmc);
-            xvar->SetFitSamples(fitsamples);
-            xvar->AddTags(tags);
-            // CCQENu::VariableFromConfig *yvar = new CCQENu::VariableFromConfig(config1D.GetValue(xvar_name));
-            CCQENu::VariableFromConfig *yvar = new CCQENu::VariableFromConfig(config1D.GetValue(yvar_name));
-            yvar->SetDoResolution(doresolution);
-            yvar->SetDoTypes(dotypes);
-            yvar->SetTunedMC(tunedmc);
-            yvar->SetFitSamples(fitsamples);
-            yvar->AddTags(tags);
-
-            
-            std::vector<std::string> fors = {};
-            if (varconfig.IsMember("for")) {
+            name2D = varconfig.GetString("name");
+            xvar_name = varconfig.GetString("xvar");
+            yvar_name = varconfig.GetString("yvar");
+            if (varconfig.IsMember("for"))
                 fors = varconfig.GetStringVector("for");
-            } else {
-                if (xvar->m_for.size() == 0) {
-                    fors = xvar->m_for;
-                } else if (yvar->m_for.size() == 0) {
-                    fors = yvar->m_for;
-                } else if (xvar->m_for.size() > 0 && yvar->m_for.size() > 0) {
-                    fors = intersection(xvar->m_for, yvar->m_for);
-                }
-            }
-            CCQENu::Variable2DFromConfig *var2D = new CCQENu::Variable2DFromConfig(name2D, *xvar, *yvar, fors);
-            var2D->AddTags(tags);
-            std::cout << "GetVariables2DFromConfig: set up 2D variable " << name2D << std::endl;
-            variables2Dmap[name2D] = var2D;
-
-            delete xvar;
-            delete yvar;
-            
         } else {
-            std::cout << "Get2DVariablesFromConfig: ERROR - have requested an unimplemented 2D variable in Get2DVariablesFromConfig " << v << std::endl;
+            name2D = v;
+            // xvar_name = name2D.substr(0, name2D.find("_"));
+            // yvar_name = name2D.substr(1, name2D.find("_"));
+            xvar_name = split2D(name2D, "_")[0];
+            yvar_name = split2D(name2D, "_")[1];
+            // std::cout << ">>>>>>>>>> name2D, xvarname, yvarname: " << "\t" << name2D << "\t" << xvar_name << "\t" << yvar_name << std::endl;
+        }
+        // Everything below used to be in "if (config2D.IsMember(v))"
+        if (!config1D.IsMember(xvar_name)){
+            std::cout << "Get2DVariablesFromConfig: ERROR - have requested an unimplemented 1D variable in Get2DVariablesFromConfig " << xvar_name << std::endl;
             assert(0);
-        } 
+        }
+        if (!config1D.IsMember(yvar_name)) {
+            std::cout << "Get2DVariablesFromConfig: ERROR - have requested an unimplemented 1D variable in Get2DVariablesFromConfig " << yvar_name << std::endl;
+            assert(0);
+        }
+        // CCQENu::VariableFromConfig *xvar = new CCQENu::VariableFromConfig(config1D.GetValue(xvar_name));
+        CCQENu::VariableFromConfig *xvar = new CCQENu::VariableFromConfig(config1D.GetValue(xvar_name));
+        xvar->SetDoResolution(doresolution);
+        xvar->SetDoTypes(dotypes);
+        xvar->SetTunedMC(tunedmc);
+        xvar->SetFitSamples(fitsamples);
+        xvar->AddTags(tags);
+        // CCQENu::VariableFromConfig *yvar = new CCQENu::VariableFromConfig(config1D.GetValue(xvar_name));
+        CCQENu::VariableFromConfig *yvar = new CCQENu::VariableFromConfig(config1D.GetValue(yvar_name));
+        yvar->SetDoResolution(doresolution);
+        yvar->SetDoTypes(dotypes);
+        yvar->SetTunedMC(tunedmc);
+        yvar->SetFitSamples(fitsamples);
+        yvar->AddTags(tags);
+
+        
+        // std::vector<std::string> fors = {};
+        // if (varconfig.IsMember("for")) {
+        //     fors = varconfig.GetStringVector("for");
+        // } else {
+        if (fors.size() == 0){
+            if (xvar->m_for.size() == 0) {
+                fors = xvar->m_for;
+            } else if (yvar->m_for.size() == 0) {
+                fors = yvar->m_for;
+            } else if (xvar->m_for.size() > 0 && yvar->m_for.size() > 0) {
+                fors = intersection(xvar->m_for, yvar->m_for);
+            }
+        }
+        CCQENu::Variable2DFromConfig *var2D = new CCQENu::Variable2DFromConfig(name2D, *xvar, *yvar, fors);
+        var2D->AddTags(tags);
+        std::cout << "GetVariables2DFromConfig: set up 2D variable " << name2D << std::endl;
+        variables2Dmap[name2D] = var2D;
+
+        delete xvar;
+        delete yvar;
+        
+        // } else {
+
+        //     std::cout << "Get2DVariablesFromConfig: ERROR - have requested an unimplemented 2D variable in Get2DVariablesFromConfig " << v << std::endl;
+        //     assert(0);
+        // } 
     }
     return variables2Dmap;
 }
--- make_hists/include/GetHyperDVariablesFromConfig.cxx
+++ make_hists/include/GetHyperDVariablesFromConfig.cxx
@@ -7,6 +7,22 @@
 #include "VariableHyperDFromConfig.h"
 #include "utils/NuConfig.h"
 
+std::vector<std::string> splitHD(std::string s, std::string delimiter) {
+    size_t pos_start = 0, pos_end, delim_len = delimiter.length();
+    std::string token;
+    std::vector<std::string> res;
+
+    while ((pos_end = s.find(delimiter, pos_start)) != std::string::npos) {
+        token = s.substr(pos_start, pos_end - pos_start);
+        pos_start = pos_end + delim_len;
+        res.push_back(token);
+    }
+
+    res.push_back(s.substr(pos_start));
+    return res;
+}
+
+
 // return a vector instead of a map (which is unneeded nowadays)
 std::map<std::string, CCQENu::VariableHyperDFromConfig *> GetHyperDVariablesFromConfig(std::vector<std::string> varsHD, std::map<std::string, CCQENu::VariableFromConfig *> variablesmap, const std::vector<std::string> tags, const NuConfig configraw) {
     //=========================================
@@ -143,8 +159,9 @@ std::map<std::string, CCQENu::VariableHyperDFromConfig *> GetHyperDVariablesFrom
     if (configraw.IsMember("HyperD")) {
         configHD = configraw.GetValue("HyperD");
     } else {
-        std::cout << "GetHyperDVariablesFromConfig: Warning: No HyperD variables configured." << std::endl;
-        exit(1);
+        // std::cout << "GetHyperDVariablesFromConfig: Warning: No HyperD variables configured." << std::endl;
+        std::cout << "GetHyperDVariablesFromConfig: Warning: No HyperD variables configured. Will try to build from 1D vars.." << std::endl;
+        // exit(1);
     }
     // Get the configs for all 1D variables in your Variables config (these likely contain more info than hyperD that you need)
     NuConfig config1D = Json::Value::null;
@@ -157,62 +174,91 @@ std::map<std::string, CCQENu::VariableHyperDFromConfig *> GetHyperDVariablesFrom
 
     std::vector<std::string> keys = configHD.GetKeys();
     for (auto v : varsHD) {
-        if (!configHD.IsMember(v)) {
-            std::cout << "GetHDVariablesFromConfig: ERROR - have requested an unimplemented HyperDim variable in GetHDVariablesFromConfig " << v << std::endl;
-            assert(configHD.IsMember(v));
-        } else {
-            NuConfig varconfig = configHD.GetValue(v);
-            std::string nameHD = varconfig.GetString("name");
-            std::vector<std::string> axisvarnamevec = varconfig.GetStringVector("vars");
-            int dim = axisvarnamevec.size();
+        std::string nameHD = "";
+        std::vector<std::string> axisvarnamevec = {};
+        EAnalysisType analysis_type = k1D;
+        bool _found_config = false;
+        NuConfig varconfig = Json::Value::null;
+        std::vector<std::string> fors = {};
+        // if (!configHD.IsMember(v)) {
+        // std::cout << "GetHDVariablesFromConfig: ERROR - have requested an unimplemented HyperDim variable in GetHDVariablesFromConfig " << v << std::endl;
+        // assert(configHD.IsMember(v));
+        // } else {
+        if (configHD.IsMember(v)) {
+            varconfig = configHD.GetValue(v);
+            // std::string nameHD = varconfig.GetString("name");
+            // std::vector<std::string> axisvarnamevec = varconfig.GetStringVector("vars");
+            nameHD = varconfig.GetString("name");
+            axisvarnamevec = varconfig.GetStringVector("vars");
+            // int dim = axisvarnamevec.size();
 
-            EAnalysisType analysis_type = k1D;
+            // EAnalysisType analysis_type = k1D;
             if (varconfig.IsMember("analysistype")) analysis_type = (EAnalysisType)varconfig.GetInt("analysistype");
+            _found_config = true;
+        } else {
+            nameHD = v;
+            axisvarnamevec = splitHD(nameHD, "_");
+        }
 
-            // std::vector<std::unique_ptr<CCQENu::VariableFromConfig>> vars1D = {};
-            std::vector<CCQENu::VariableFromConfig *> vars1D = {};
-            std::vector<std::string> tmp_fors = {};  // What you want to fill hists of (e.g. data, selected_reco), defaults to all<-TODO in VariableHyperDFromConfig
+        int dim = axisvarnamevec.size();
+        if (dim == 0) {
+            std::cout << "ERROR: didn't set up HD var correctly " << nameHD << std::endl;
+            assert(0);
+        }
+        std::vector<CCQENu::VariableFromConfig *> vars1D = {};
+        std::vector<std::string> tmp_fors = {};  // What you want to fill hists of (e.g. data, selected_reco), defaults to all<-TODO in VariableHyperDFromConfig
 
-            for (auto var1D_name : axisvarnamevec) {
-                if (!config1D.IsMember(var1D_name)) {
-                    std::cout << "GetHDVariablesFromConfig: ERROR - have requested an unimplemented 1D variable in GetHDVariablesFromConfig " << var1D_name << std::endl;
-                    assert(0);
-                }
-                CCQENu::VariableFromConfig *tmp_var1D = new CCQENu::VariableFromConfig(config1D.GetValue(var1D_name));
-
-                tmp_var1D->SetDoResolution(doresolution);
-                tmp_var1D->SetDoTypes(dotypes);
-                tmp_var1D->SetTunedMC(tunedmc);
-                tmp_var1D->SetFitSamples(fitsamples);
-                tmp_var1D->AddTags(tags);
-
-                for (auto f : tmp_var1D->m_for)
-                    if (std::find(tmp_fors.begin(), tmp_fors.end(), f) != tmp_fors.end()) tmp_fors.push_back(f);
-
-                vars1D.emplace_back(tmp_var1D);
+        for (auto var1D_name : axisvarnamevec) {
+            if (!config1D.IsMember(var1D_name)) {
+                std::cout << "GetHDVariablesFromConfig: ERROR - have requested an unimplemented 1D variable in GetHDVariablesFromConfig " << var1D_name << std::endl;
+                assert(0);
             }
-            for (auto var1D_name : axisvarnamevec) {
-                if (!config1D.IsMember(var1D_name)) {
-                    std::cout << "GetHDVariablesFromConfig: ERROR - have requested an unimplemented 1D variable in GetHDVariablesFromConfig " << var1D_name << std::endl;
-                    assert(0);
-                }
-                CCQENu::VariableFromConfig *tmp_var1D = new CCQENu::VariableFromConfig(config1D.GetValue(var1D_name));
-                std::vector<std::string> var1d_fors = tmp_var1D->m_for;
-                for (auto f : tmp_fors) {
-                    if (std::find(var1d_fors.begin(), var1d_fors.end(), f) != var1d_fors.end())
-                        tmp_fors.erase(std::find(tmp_fors.begin(), tmp_fors.end(), f));
+            CCQENu::VariableFromConfig *tmp_var1D = new CCQENu::VariableFromConfig(config1D.GetValue(var1D_name));
+
+            tmp_var1D->SetDoResolution(doresolution);
+            tmp_var1D->SetDoTypes(dotypes);
+            tmp_var1D->SetTunedMC(tunedmc);
+            tmp_var1D->SetFitSamples(fitsamples);
+            tmp_var1D->AddTags(tags);
+
+            for (auto f : tmp_var1D->m_for) {
+                if (std::count(tmp_fors.begin(), tmp_fors.end(), f) == 0) {
+                    tmp_fors.push_back(f);
                 }
             }
-            std::vector<std::string> fors = {};  // What you want to fill hists of (e.g. data, selected_reco), defaults to all<-TODO in VariableHyperDFromConfig
-            fors = varconfig.IsMember("for") ? varconfig.GetStringVector("for"): tmp_fors;
+            vars1D.emplace_back(tmp_var1D);
+        }
+        // Need to go back over to erase the fors that aren't shared between each 1D variable
+        std::vector<std::string> new_fors = tmp_fors;  // What you want to fill hists of (e.g. data, selected_reco), defaults to all<-TODO in VariableHyperDFromConfig
 
-            CCQENu::VariableHyperDFromConfig *varHD = new CCQENu::VariableHyperDFromConfig(nameHD, vars1D, fors, analysis_type);
-            varHD->AddTags(tags);
-            std::cout << "GetHyperDVariablesFromConfig: set up HyperDim variable " << nameHD << " of dimension " << dim << std::endl;
-            variablesHDmap[nameHD] = varHD;
-            for (auto var1D: vars1D) {
-                delete var1D;
+        for (auto var : vars1D) {
+            std::vector<std::string> var1d_fors = var->m_for;
+            for (auto f : tmp_fors) {
+                if (std::count(var1d_fors.begin(), var1d_fors.end(), f) == 0){
+                    new_fors.erase(std::find(new_fors.begin(), new_fors.end(), f));
+                }
             }
+        }
+        // for (auto var1D_name : axisvarnamevec) {
+        //     CCQENu::VariableFromConfig *tmp_var1D = new CCQENu::VariableFromConfig(config1D.GetValue(var1D_name));
+        //     std::vector<std::string> var1d_fors = tmp_var1D->m_for;
+        //     for (auto f : tmp_fors) {
+        //         if (std::find(var1d_fors.begin(), var1d_fors.end(), f) != var1d_fors.end())
+        //             tmp_fors.erase(std::find(tmp_fors.begin(), tmp_fors.end(), f));
+        //     }
+        // }
+        fors = new_fors;
+        // std::vector<std::string> fors = {};  // What you want to fill hists of (e.g. data, selected_reco), defaults to all<-TODO in VariableHyperDFromConfig
+        if (_found_config) fors = varconfig.IsMember("for") ? varconfig.GetStringVector("for"): tmp_fors;
+        std::cout << ">>>>>>> fors found: ";
+        for (auto f : fors) std::cout << "\t" << f;
+        std::cout << std::endl;
+        CCQENu::VariableHyperDFromConfig *varHD = new CCQENu::VariableHyperDFromConfig(nameHD, vars1D, fors, analysis_type);
+        varHD->AddTags(tags);
+        std::cout << "GetHyperDVariablesFromConfig: set up HyperDim variable " << nameHD << " of dimension " << dim << std::endl;
+        variablesHDmap[nameHD] = varHD;
+        for (auto var1D: vars1D) {
+            delete var1D;
         }
     }
     return variablesHDmap;
