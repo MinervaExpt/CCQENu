f4c20f8f10eea2293cb972206091586a21e6c7c5
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2025-11-17 16:56:04 -0800

    do simulfit

--- utilities/run_bkgfitfill_p6.sh
+++ utilities/run_bkgfitfill_p6.sh
@@ -14,7 +14,7 @@
 export MYWARP=none
 export MYMODEL=MnvTunev1
 
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/bkgfitting/eventloopout/November2025/bkgfit_newprotonscore_recoil0mm_fullsyst \
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/bkgfitting/eventloopout/November2025/fitfill_bkgfit_newprotonscore_recoil0mm_fullsyst \
  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=${MYMODEL} --warp=${MYWARP} --tag=bkgfitting_recoil0mm \
  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid_bkgsub --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=24h --memory=5000   #--sample=${MYSAMPLE} #--debug --notimestamp
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=36h --memory=5000   #--sample=${MYSAMPLE} #--debug --notimestamp
