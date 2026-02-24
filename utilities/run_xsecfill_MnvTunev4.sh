c7535c210dec49cb6216468ce25fd2ef88a06a60
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2026-02-23 22:07:33 -0800

    fixed sub reqs

--- utilities/run_xsecfill_MnvTunev4.sh
+++ utilities/run_xsecfill_MnvTunev4.sh
@@ -18,4 +18,4 @@ export MYMODEL=MnvTunev4.3.1
 python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/eventloopout/February2026/xsec/recoil100mm500MeVcut_200flux/${MYMODEL} \
  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=${MYMODEL} --warp=${MYWARP} --tag=${MYMODEL}_xsec_xtract \
  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
- --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=4000   --sample=${MYSAMPLE} #--debug --notimestamp
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=36h --memory=6000   --sample=${MYSAMPLE} #--debug --notimestamp
