1c68d2d546c7693983bc2660dfe940d79cceaa82
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2026-02-23 21:28:01 -0800

    script to run for xsec xtraction

---
+++ utilities/run_xsecfill_MnvTunev4.sh
@@ -0,0 +1,21 @@
+#export _CONDOR_SCRATCH_DIR=$PWD
+#export INPUT_TAR_DIR_LOCAL=$APP
+
+# example test batch job to run on minervagpvm01 to run CCQEMAT
+# other option (not tested yet) is the EventLoop from the tutorial
+
+# type python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py to see the option descriptions
+# here my release is in $APP/NEWMAT - your mileage may differ
+
+# ==========
+
+# QElike
+export MYSAMPLE=QElike
+export MYWARP=none
+
+
+export MYMODEL=MnvTunev4.3.1
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/eventloopout/February2026/xsec/recoil100mm500MeVcut_200flux/${MYMODEL} \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=${MYMODEL} --warp=${MYWARP} --tag=${MYMODEL}_xsec_xtract \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=4000   --sample=${MYSAMPLE} #--debug --notimestamp
