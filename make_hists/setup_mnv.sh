faf6b2718bf86e4c7c01df0f4556066385379277
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2026-03-06 14:19:59 -0800

    try anu public

--- make_hists/setup_mnv.sh
+++ make_hists/setup_mnv.sh
@@ -13,4 +13,9 @@ export CCQEMAT=$WHEREIPUTMYCODE/CCQENu/make_hists
 #export DYLD_LIBRARY_PATH=${PLOTUTILSROOT}
 export PYTHONPATH=$CCQEMAT/python:$WHEREIPUTMYCODE/MAT-MINERvA/python:$WHEREIPUTMYCODE/MAT-MINERvA/python/PlotUtils:$PYTHONPATH
 sed s+GIT_COMMIT_HASH+`git rev-parse --verify HEAD`+ utils/gitVersion.h.in > utils/gitVersion.h
-export THEDATA=/pnfs/minerva/persistent/users/drut1186/CCQENu_Anatuples/MuonKludge_ProtonLLR_UpdatedNeutron
+#export THEDATA=/pnfs/minerva/persistent/users/drut1186/CCQENu_Anatuples/MuonKludge_ProtonLLR_UpdatedNeutron
+export MYPLAYLIST="5A"
+export PRESCALE=100
+export DATALOC=remote
+export MYMODEL=MnvTunev2
+export MYSAMPLE=QElike
