ebdfeccd3de0bf199f9f3cb3a6c500d5e05be9f2
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2026-05-26 13:19:41 -0700

    change grid scripts

--- utilities/run_bkgfitfill_p6_MnvTunev2.0.1.sh
+++ utilities/run_bkgfitfill_p6_MnvTunev2.0.1.sh
@@ -14,7 +14,7 @@
 export MYWARP=none
 export MYMODEL=MnvTunev2.0.1
 
-python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/eventloopout/May2026/bkgfitting/${MYMODEL}/TESTREBINNOFLUX_fix_geanthadroncv_fitfill_bkgfit_200flux_p6_${MYMODEL} \
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/eventloopout/May2026/bkgfitting/${MYMODEL}/Rebin_fix_geanthadroncv_fitfill_bkgfit_200flux_p6_${MYMODEL} \
  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=${MYMODEL} --warp=${MYWARP} --tag=bkgfitting_${MYMODEL} \
  --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_grid_bkgsub --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
  --tmpdir=$SCRATCH/tmp --expected-lifetime=36h --memory=5000   #--sample=${MYSAMPLE} #--debug --notimestamp
