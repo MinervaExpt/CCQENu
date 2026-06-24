8f157e96f7c440c505179caf3d1c4931aee69599
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2026-06-24 13:19:47 -0700

    fix sub script to do p8

--- utilities/run_neutplots_p8.sh
+++ utilities/run_neutplots_p8.sh
@@ -16,10 +16,10 @@ export MYMODEL=MnvTunev2.0.1
 
 python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/eventloopout/June2026/blobstudy_${MYMODEL}/1and2track_allblobs/twodim \
  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=${MYMODEL} --warp=${MYWARP} --tag=${MYMODEL}_neutronblob_plotstuff_hd \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_hdneut --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_hdneut --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9_p8.sh \
  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=3000   --sample=${MYSAMPLE} #--debug --notimestamp
 
 python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/eventloopout/June2026/blobstudy_${MYMODEL}/1and2track_allblobs/onedim \
  --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=${MYMODEL} --warp=${MYWARP} --tag=${MYMODEL}_neutronblob_plotstuff_1d \
- --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_1dneut --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_1dneut --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9_p8.sh \
  --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=3000   --sample=${MYSAMPLE} #--debug --notimestamp
