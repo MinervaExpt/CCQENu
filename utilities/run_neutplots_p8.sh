2180c2beafcad79cbd6e8f013a8fbe734ca090fe
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2026-06-24 12:54:21 -0700

    stuff for running 1D

---
+++ utilities/run_neutplots_p8.sh
@@ -0,0 +1,25 @@
+#export _CONDOR_SCRATCH_DIR=$PWD
+#export INPUT_TAR_DIR_LOCAL=$APP
+
+# example test batch job to run on minervagpvm01 to run CCQEMAT
+# other option (not tested yet) is the EventLoop from the tutorial
+
+# type python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py to see the option descriptions
+# here my release is in $APP/NEWMAT - your mileage may differ
+
+# ==========
+
+# QElike
+export MYSAMPLE=QElike
+export MYWARP=none
+export MYMODEL=MnvTunev2.0.1
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/eventloopout/June2026/blobstudy_${MYMODEL}/1and2track_allblobs/twodim \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=${MYMODEL} --warp=${MYWARP} --tag=${MYMODEL}_neutronblob_plotstuff_hd \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_hdneut --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=3000   --sample=${MYSAMPLE} #--debug --notimestamp
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/eventloopout/June2026/blobstudy_${MYMODEL}/1and2track_allblobs/onedim \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=minervame5A --model=${MYMODEL} --warp=${MYWARP} --tag=${MYMODEL}_neutronblob_plotstuff_1d \
+ --mail --prescale=1 --config=nhv/config/warpingstudies/AntiNu_v15_warping_1dneut --exe=sidebands_v2 --setup=CCQENu/utilities/setup_batch_mat9.sh \
+ --tmpdir=$SCRATCH/tmp --expected-lifetime=4h --memory=3000   --sample=${MYSAMPLE} #--debug --notimestamp
