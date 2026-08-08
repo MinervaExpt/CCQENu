614f72c7a251de3dcaf3cbbbcae570936d789baf
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2026-08-08 15:45:39 -0700

    p7 check

---
+++ utilities/proto_p7.sh
@@ -0,0 +1,14 @@
+#export _CONDOR_SCRATCH_DIR=$PWD
+export INPUT_TAR_DIR_LOCAL=/exp/minerva/data/users/$USER
+
+# example test batch job to run on minervagpvm01 to run CCQEMAT
+# other option (not tested yet) is the EventLoop from the tutorial
+
+# type python $APP/NEWMAT/CCQENu/utilities/SubmitJobsToGrid_MAT.py to see the option descriptions
+# here my release is in $APP/NEWMAT - your mileage may differ
+
+
+python $WHEREIPUTMYCODE/CCQENu/utilities/SubmitJobsToGrid_MAT.py --stage=CCQEMAT --outdir=$SCRATCH/test \
+ --basedir=$WHEREIPUTMYCODE --rundir=CCQENu/make_hists --playlist=5A --model=MnvTunev2 --tag=AL9-p7 \
+ --mail --prescale=100 --config=p7_run --exe=sidebands_v2 --sample=QElike --setup=CCQENu/utilities/setup_batch_al9_proto.sh \
+ --tmpdir=/exp/minerva/data/users/$USER/tmp --expected-lifetime=4h --memory=3000   #--debug --notimestamp
