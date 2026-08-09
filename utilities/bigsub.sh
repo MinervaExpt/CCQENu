0105b5dd001183e212f0d0c2e2a879a5d8e1d9f5
Heidi Schellman <33669005+hschellman@users.noreply.github.com>
2026-08-08 19:02:28 -0700

    adapt bigsub for new naming

--- utilities/bigsub.sh
+++ utilities/bigsub.sh
@@ -1,15 +1,15 @@
 # make a big submission script
 
 #touch big$1
-sed s/minervame5A/minervame5A/ $1 > big$1
-sed s/minervame5A/minervame6A/ $1 >> big$1
-sed s/minervame5A/minervame6B/ $1 >> big$1
-sed s/minervame5A/minervame6C/ $1 >> big$1
-sed s/minervame5A/minervame6D/ $1 >> big$1
-sed s/minervame5A/minervame6E/ $1 >> big$1
-sed s/minervame5A/minervame6F/ $1 >> big$1
-sed s/minervame5A/minervame6G/ $1 >> big$1
-sed s/minervame5A/minervame6H/ $1 >> big$1
-sed s/minervame5A/minervame6I/ $1 >> big$1
-sed s/minervame5A/minervame6J/ $1 >> big$1
+sed s/5A/5A/ $1 > big$1
+sed s/5A/6A/ $1 >> big$1
+sed s/5A/6B/ $1 >> big$1
+sed s/5A/6C/ $1 >> big$1
+sed s/5A/6D/ $1 >> big$1
+sed s/5A/6E/ $1 >> big$1
+sed s/5A/6F/ $1 >> big$1
+sed s/5A/6G/ $1 >> big$1
+sed s/5A/6H/ $1 >> big$1
+sed s/5A/6I/ $1 >> big$1
+sed s/5A/6J/ $1 >> big$1
 
