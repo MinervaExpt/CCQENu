44ed08949d4b4898601850fe9dbc6b09864294e8
Noah Harvey Vaughan <noah.vaughan31@gmail.com>
2026-08-17 13:50:22 -0700

    fix it again tony

--- make_hists/include/NeutronMultiplicity.cxx
+++ make_hists/include/NeutronMultiplicity.cxx
@@ -850,8 +850,8 @@ bool NeutEvent::CandPassVtxDist(int index) {
             return dist >= 79.195 * log(edep) - 171.93;
         }
         // if (m_cands[index]->GetCandRecoEDep() < 12.0) return true;
-        return dist >= m_vtxdist_min;
-    }
+    return dist >= m_vtxdist_min;
+    // }
 }
 
 bool NeutEvent::CandPassVtxZDist(int index) {
